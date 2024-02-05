a812356216199f6968edb7cb2190240202f7cb8c
inclined4X <jezemiahsam48@gmail.com>
2024-02-05 15:41:58 +0000

    changes to html and script file

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -2,9 +2,18 @@ const plusIcons = document.querySelectorAll(".plus");
 const minusIcons = document.querySelectorAll(".cross");
 const paragraphs = document.querySelectorAll(".paragraph");
 
-// for loop
+// for loop for when clicked on the plus icon
 for (let i = 0; i < plusIcons.length; i++) {
   plusIcons[i].addEventListener("click", function () {
+    plusIcons[i].classList.toggle("hidden");
+    minusIcons[i].classList.toggle("hidden");
+    paragraphs[i].classList.toggle("hidden");
+  });
+}
+
+// for loop for when clicked on the plus icon
+for (let i = 0; i < minusIcons.length; i++) {
+  minusIcons[i].addEventListener("click", function () {
     plusIcons[i].classList.toggle("hidden");
     minusIcons[i].classList.toggle("hidden");
     paragraphs[i].classList.toggle("hidden");
