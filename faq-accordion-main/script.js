36a0e3fc516c968e190ef3a944fc386832ba0ee6
inclined4X <jezemiahsam48@gmail.com>
2024-02-05 15:39:15 +0000

    finished up the functionality for when the  plus icon is clicked on

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -1,3 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const faqContainer = document.querySelector("faq-accordion");
-});
+const plusIcons = document.querySelectorAll(".plus");
+const minusIcons = document.querySelectorAll(".cross");
+const paragraphs = document.querySelectorAll(".paragraph");
+
+// for loop
+for (let i = 0; i < plusIcons.length; i++) {
+  plusIcons[i].addEventListener("click", function () {
+    plusIcons[i].classList.toggle("hidden");
+    minusIcons[i].classList.toggle("hidden");
+    paragraphs[i].classList.toggle("hidden");
+  });
+}
